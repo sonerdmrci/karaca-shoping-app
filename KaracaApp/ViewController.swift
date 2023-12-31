d375622c7ae7841e922c5d5639301ba1f9096722
Soner DEMİRCİ <soner.demirci@tein.com.tr>
2023-12-31 12:20:54 +0300

    Initial Commit

---
+++ KaracaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KaracaApp
+//
+//  Created by Soner Demirci on 31.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
